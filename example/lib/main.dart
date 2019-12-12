9e4eb3f183a525ae94e5e675f4e62e4084cba9b8
Peter Michaux <petermichaux@gmail.com>
2019-12-12 11:07:50 -0800

    Expose more information about the downloaded file in the example app's UI. Clear the displayed file info when the cache is cleared. Show a progress indicator while downloading.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,46 +1,51 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Flutter Cache Manager Demo',
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool loading = false;
   FileInfo fileInfo;
   String error;
 
   _downloadFile() {
-    var url =
-        'https://cdn2.online-convert.com/example-file/raster%20image/png/example_small.png';
+    if (loading) {
+      return;
+    }
+
+    setState(() {
+      loading = true;
+      fileInfo = null;
+      error = null;
+    });
+
+    var url = 'https://cdn2.online-convert.com/example-file/raster%20image/png/example_small.png';
 
     DefaultCacheManager().getFile(url).listen((f) {
       setState(() {
+        loading = false;
         fileInfo = f;
         error = null;
       });
     }).onError((e) {
       setState(() {
+        loading = false;
         fileInfo = null;
         error = e.toString();
       });
@@ -49,49 +54,100 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    var path = "N/A";
-    if (fileInfo?.file != null) {
-      path = fileInfo.file.path;
-    }
-    var from = "N/A";
-    if (fileInfo != null) {
-      from = fileInfo.source.toString();
-    }
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
+    Widget body;
+    Widget fab;
+
+    if (loading) {
+      body = Center(
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Local filePath:',
+            Container(
+              width: 50.0,
+              height: 50.0,
+              child: CircularProgressIndicator(),
             ),
-            Text(
-              path,
-            ),
-            Text(
-              'From: $from',
-            ),
-            Text('Error: $error'),
-            Padding(
-              padding: const EdgeInsets.only(top: 32.0),
-              child: RaisedButton(
-                child: Text('CLEAR CACHE'),
-                onPressed: () {
-                  DefaultCacheManager().emptyCache();
-                },
-              ),
-            )
+            SizedBox(width: 20.0),
+            Text("Downloading"),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
+      );
+    } else {
+      final List<Widget> children = [];
+      if (fileInfo != null) {
+        if (fileInfo != null) {
+          children.add(
+            ListTile(
+              title: Text("Original URL"),
+              subtitle: Text(fileInfo.originalUrl),
+            ),
+          );
+
+          if (fileInfo.file != null) {
+            File file = fileInfo.file;
+            children.add(
+              ListTile(
+                title: Text("Local file path"),
+                subtitle: Text(file.path),
+              ),
+            );
+          }
+
+          children.add(
+            ListTile(
+              title: Text("Loaded from"),
+              subtitle: Text(fileInfo.source.toString()),
+            ),
+          );
+
+          children.add(
+            ListTile(
+              title: Text("Valid Until"),
+              subtitle: Text(fileInfo.validTill.toIso8601String()),
+            ),
+          );
+        }
+
+        if (error != null) {
+          ListTile(
+            title: Text("Error"),
+            subtitle: Text(error.toString()),
+          );
+        }
+
+        children.add(
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: RaisedButton(
+              child: Text('CLEAR CACHE'),
+              onPressed: () {
+                DefaultCacheManager().emptyCache();
+                this.setState(() {
+                  fileInfo = null;
+                });
+              },
+            ),
+          ),
+        );
+      } else {
+        children.add(ListTile(title: Text("Tap the floating action button to download.")));
+      }
+
+      body = ListView(children: children);
+
+      fab = FloatingActionButton(
         onPressed: _downloadFile,
         tooltip: 'Download',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Icon(Icons.cloud_download),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Cache Manager Demo"),
+      ),
+      body: body,
+      floatingActionButton: fab,
     );
   }
 }
