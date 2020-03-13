fefc4d0fa0a53aa010fbcbae8008285fa01b7489
Rene Floor <r.floor.1@gmail.com>
2020-03-13 17:55:42 +0100

    Upgrade FileFetcher to a FileService
    Introduces option for 'post' in the future.
    HttpFileFetcherResponse handles all logic of headers, so a FileFetcherResponse doesn't need to magic headers anymore.
    BodyBytes is changed to Content and is a stream now.

--- lib/src/file_fetcher.dart
+++ lib/src/file_fetcher.dart
@@ -1,42 +1,92 @@
 import 'dart:async';
-import 'dart:typed_data';
-
 import 'package:http/http.dart' as http;
 
 ///Flutter Cache Manager
 ///Copyright (c) 2019 Rene Floor
 ///Released under MIT License.
 
-typedef Future<FileFetcherResponse> FileFetcher(String url, {Map<String, String> headers});
+abstract class FileService {
+  Future<FileFetcherResponse> get(String url, {Map<String, String> headers});
+}
+
+class HttpFileFetcher implements FileService {
+  final http.Client _httpClient = http.Client();
+
+  @override
+  Future<FileFetcherResponse> get(String url,
+      {Map<String, String> headers}) async {
+    final req = http.Request('GET', Uri.parse(url));
+    req.headers.addAll(headers);
+    final httpResponse = await _httpClient.send(req);
+
+    return HttpFileFetcherResponse(httpResponse);
+  }
+}
 
 abstract class FileFetcherResponse {
+  Stream<List<int>> get content => null;
   int get statusCode;
-
-  bool hasHeader(String name);
-
-  String header(String name);
-
-  Uint8List get bodyBytes => null;
+  DateTime get validTill;
+  String get eTag;
+  String get fileExtension;
 }
 
 class HttpFileFetcherResponse implements FileFetcherResponse {
-  const HttpFileFetcherResponse(this._response);
+  HttpFileFetcherResponse(this._response);
 
-  final http.Response _response;
+  final DateTime _receivedTime = DateTime.now();
+
+  final http.StreamedResponse _response;
 
   @override
   int get statusCode => _response.statusCode;
 
-  @override
-  bool hasHeader(String name) {
+  bool _hasHeader(String name) {
     return _response.headers.containsKey(name);
   }
 
-  @override
-  String header(String name) {
+  String _header(String name) {
     return _response.headers[name];
   }
 
   @override
-  Uint8List get bodyBytes => _response.bodyBytes;
+  Stream<List<int>> get content => _response.stream;
+
+  @override
+  DateTime get validTill {
+    // Without a cache-control header we keep the file for a week
+    var ageDuration = const Duration(days: 7);
+    if (_hasHeader('cache-control')) {
+      final controlSettings = _header('cache-control').split(',');
+      for (final setting in controlSettings) {
+        final sanitizedSetting = setting.trim().toLowerCase();
+        if (sanitizedSetting == 'no-cache') {
+          ageDuration = const Duration();
+        }
+        if (sanitizedSetting.startsWith('max-age=')) {
+          var validSeconds = int.tryParse(sanitizedSetting.split('=')[1]) ?? 0;
+          if (validSeconds > 0) {
+            ageDuration = Duration(seconds: validSeconds);
+          }
+        }
+      }
+    }
+
+    return _receivedTime.add(ageDuration);
+  }
+
+  @override
+  String get eTag => _hasHeader('etag') ? _header('etag') : null;
+
+  @override
+  String get fileExtension {
+    var fileExtension = '';
+    if (_hasHeader('content-type')) {
+      final type = _header('content-type').split('/');
+      if (type.length == 2) {
+        fileExtension = '.${type[1]}';
+      }
+    }
+    return fileExtension;
+  }
 }
