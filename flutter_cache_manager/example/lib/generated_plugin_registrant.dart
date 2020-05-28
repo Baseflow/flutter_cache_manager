2331c9799dcf03ef1e821b417b58ffe94ef0fcdd
Rene Floor <r.floor.1@gmail.com>
2020-05-28 14:11:20 +0200

    move package to subdir

---
+++ flutter_cache_manager/example/lib/generated_plugin_registrant.dart
@@ -0,0 +1,16 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore: unused_import
+import 'dart:ui';
+
+import 'package:firebase_core_web/firebase_core_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(PluginRegistry registry) {
+  FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
+  registry.registerMessageHandler();
+}
