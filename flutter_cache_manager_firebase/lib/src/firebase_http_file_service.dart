63e2139706b234c5801c9127deafeb139bec4715
Nolence <rex.magana@gmail.com>
2020-10-21 11:26:58 -0600

    Replace getDownloadURL with getData

--- flutter_cache_manager_firebase/lib/src/firebase_http_file_service.dart
+++ flutter_cache_manager_firebase/lib/src/firebase_http_file_service.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:http/http.dart' as http;
 
 /// [FirebaseHttpFileService] is another common file service which parses a
 /// firebase reference into, to standard url which can be passed to the
@@ -8,9 +9,11 @@ class FirebaseHttpFileService extends HttpFileService {
   @override
   Future<FileServiceResponse> get(String url,
       {Map<String, String> headers = const {}}) async {
-    var ref = FirebaseStorage.instance.ref().child(url);
-    var _url = await ref.getDownloadURL() as String;
+    final ref = await FirebaseStorage.instance.getReferenceFromUrl(url);
+    final streamedResponse = http.StreamedResponse(
+        ref.getData(0x12FFFFFF).asStream(), 200,
+        headers: headers);
 
-    return super.get(_url);
+    return HttpGetResponse(streamedResponse);
   }
 }
