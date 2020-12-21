1c64757efbdc9957352c237cbeba8d0754b68e6f
Rene Floor <r.floor.1@gmail.com>
2020-12-21 15:38:43 +0100

    Update iOS example files

--- flutter_cache_manager/example/lib/generated_plugin_registrant.dart
+++ flutter_cache_manager/example/lib/generated_plugin_registrant.dart
@@ -2,12 +2,15 @@
 // Generated file. Do not edit.
 //
 
+// ignore: unused_import
+import 'dart:ui';
+
 import 'package:url_launcher_web/url_launcher_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
-void registerPlugins(Registrar registrar) {
-  UrlLauncherPlugin.registerWith(registrar);
-  registrar.registerMessageHandler();
+void registerPlugins(PluginRegistry registry) {
+  UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
+  registry.registerMessageHandler();
 }
