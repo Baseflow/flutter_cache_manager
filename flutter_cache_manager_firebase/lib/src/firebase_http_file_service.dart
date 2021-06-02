f701f63f00c8183d0337c50da3416c60aac4d0f1
Nolence <rex.magana@gmail.com>
2021-06-02 14:53:14 -0600

    Use metadata and getData in favor of long lived url

--- flutter_cache_manager_firebase/lib/src/firebase_http_file_service.dart
+++ flutter_cache_manager_firebase/lib/src/firebase_http_file_service.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:http/http.dart' as http;
 
 /// [FirebaseHttpFileService] is another common file service which parses a
 /// firebase reference into, to standard url which can be passed to the
@@ -8,9 +12,31 @@ class FirebaseHttpFileService extends HttpFileService {
   @override
   Future<FileServiceResponse> get(String url,
       {Map<String, String>? headers}) async {
-    var ref = FirebaseStorage.instance.ref().child(url);
-    var _url = await ref.getDownloadURL();
+    final ref = FirebaseStorage.instance.refFromURL(url);
+    final metaData = await ref.getMetadata();
+    final contentType = metaData.contentType;
+    final contentLanguage = metaData.contentLanguage;
+    final date = metaData.timeCreated?.millisecondsSinceEpoch;
+    final cacheControl = metaData.cacheControl;
+    final headers = <String, String>{
+      if (contentType != null) HttpHeaders.contentTypeHeader: contentType,
+      if (contentLanguage != null)
+        HttpHeaders.contentLanguageHeader: contentLanguage,
+      if (date != null) HttpHeaders.dateHeader: date.toString(),
+      if (cacheControl != null) HttpHeaders.cacheControlHeader: cacheControl,
+      HttpHeaders.contentLocationHeader: metaData.fullPath,
+    };
+    final stream =
+        ref.getData(metaData.size ?? 10485760).asStream().where((event) {
+      if (event == null) print('null data for url $url');
+      return event != null;
+    }).cast<Uint8List>();
+    final response = http.StreamedResponse(
+      stream,
+      200,
+      headers: headers,
+    );
 
-    return super.get(_url);
+    return HttpGetResponse(response);
   }
 }
