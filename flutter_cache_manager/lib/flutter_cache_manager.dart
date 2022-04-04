d44294e5bcb7a837e6cfffd44e04ea47d2baf94e
Tristan Vanderaerden <Tristan.Vanderaerden@vrt.be>
2022-04-04 16:13:49 +0200

    * Fix build * Export CacheObject to allow extending CacheInfoRepository

--- flutter_cache_manager/lib/flutter_cache_manager.dart
+++ flutter_cache_manager/lib/flutter_cache_manager.dart
@@ -5,5 +5,6 @@ export 'src/config/config.dart';
 export 'src/logger.dart';
 export 'src/result/result.dart';
 export 'src/storage/cache_info_repositories/cache_info_repositories.dart';
+export 'src/storage/cache_object.dart';
 export 'src/web/file_service.dart';
 export 'src/web/web_helper.dart' show HttpExceptionWithStatus;
