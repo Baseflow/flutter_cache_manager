2e37e33a49bd0fa1af20cdc675cef887890fe4d2
ltOgt <omnesiaorg@gmail.com>
2023-12-05 13:49:58 +0100

    add only if has listener

--- flutter_cache_manager/lib/src/cache_manager.dart
+++ flutter_cache_manager/lib/src/cache_manager.dart
@@ -156,7 +156,9 @@ class CacheManager implements BaseCacheManager {
         }
 
         if (cacheFile != null && e is HttpExceptionWithStatus && e.statusCode == 404) {
-          streamController.addError(e);
+          if (streamController.hasListener) {
+            streamController.addError(e);
+          }
           await removeFile(key);
         }
       }
