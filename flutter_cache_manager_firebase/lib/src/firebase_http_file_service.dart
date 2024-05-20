b8b124672a1ae3913c42e60eaa6bbb70d16e011a
Peter Keefe <16809252+percula@users.noreply.github.com>
2024-05-19 21:50:21 -0400

    feat: Add retry mechanism (WIP)

--- flutter_cache_manager_firebase/lib/src/firebase_http_file_service.dart
+++ flutter_cache_manager_firebase/lib/src/firebase_http_file_service.dart
@@ -1,16 +1,33 @@
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:retry/retry.dart';
 
 /// [FirebaseHttpFileService] is another common file service which parses a
 /// firebase reference into, to standard url which can be passed to the
 /// standard [HttpFileService].
 class FirebaseHttpFileService extends HttpFileService {
+
+  final RetryOptions? retryOptions;
+
+  FirebaseHttpFileService({
+    this.retryOptions,
+  });
+
   @override
   Future<FileServiceResponse> get(String url,
       {Map<String, String>? headers}) async {
     var ref = FirebaseStorage.instance.ref().child(url);
-    var _url = await ref.getDownloadURL();
+    var _url;
+    if (retryOptions != null) {
+      _url = await retryOptions!.retry(
+              () => ref.getDownloadURL(),
+        retryIf: (e) => e is FirebaseException,
 
+      );
+    } else {
+      _url = await ref.getDownloadURL();
+    }
     return super.get(_url);
   }
+
 }
