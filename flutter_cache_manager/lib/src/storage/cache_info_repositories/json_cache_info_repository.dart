ae0fc03c3bbca5b905629c7d370cd7e84d92bb52
Jethro <jethro@bothyculture.org>
2025-05-04 12:23:57 +0000

    fix: check file is not empty

--- flutter_cache_manager/lib/src/storage/cache_info_repositories/json_cache_info_repository.dart
+++ flutter_cache_manager/lib/src/storage/cache_info_repositories/json_cache_info_repository.dart
@@ -137,7 +137,7 @@ class JsonCacheInfoRepository extends CacheInfoRepository
   Future<void> _readFile(File file) async {
     _cacheObjects.clear();
     _jsonCache.clear();
-    if (await file.exists()) {
+    if (await file.exists() && await file.length() > 0) { // Check if file is not empty
       try {
         final jsonString = await file.readAsString();
         final json = jsonDecode(jsonString) as List<dynamic>;
