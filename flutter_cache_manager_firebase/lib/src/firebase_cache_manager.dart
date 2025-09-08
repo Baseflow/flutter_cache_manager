b36b3f37e00a4395f17da4fd475fe1b3f8a598e4
minhdanh <minhdanh@users.noreply.github.com>
2025-09-09 06:17:29 +0700

    Fix bucket name being reused between instances of different buckets

--- flutter_cache_manager_firebase/lib/src/firebase_cache_manager.dart
+++ flutter_cache_manager_firebase/lib/src/firebase_cache_manager.dart
@@ -6,22 +6,27 @@ import 'firebase_http_file_service.dart';
 /// Use [FirebaseCacheManager] if you want to download files from firebase storage
 /// and store them in your local cache.
 class FirebaseCacheManager extends CacheManager {
-  static const key = 'firebaseCache';
+  static const defaultKey = 'firebaseCache';
 
-  static final FirebaseCacheManager _instance =
-      FirebaseCacheManager._(retryOptions: _retryOptions, bucket: _bucket);
+  static final Map<String, FirebaseCacheManager> _instances = {};
 
   static RetryOptions? _retryOptions;
 
   static String? _bucket;
 
   factory FirebaseCacheManager({RetryOptions? retryOptions, String? bucket}) {
-    _bucket = bucket;
-    _retryOptions = retryOptions;
-    return _instance;
+    final cacheKey = bucket ?? defaultKey;
+    if (_instances.containsKey(cacheKey)) {
+      return _instances[cacheKey]!;
+    }
+    _bucket = bucket ?? _bucket;
+    _retryOptions = retryOptions ?? _retryOptions;
+    final instance = FirebaseCacheManager._(key: cacheKey, retryOptions: _retryOptions, bucket: _bucket);
+    _instances[cacheKey] = instance;
+    return instance;
   }
 
-  FirebaseCacheManager._({RetryOptions? retryOptions, String? bucket})
+  FirebaseCacheManager._({required String key, RetryOptions? retryOptions, String? bucket})
       : super(Config(key,
             fileService: FirebaseHttpFileService(
                 retryOptions: retryOptions, bucket: bucket)));
