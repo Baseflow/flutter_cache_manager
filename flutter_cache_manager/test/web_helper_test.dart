1484c121776097178edd5bcbbea46aac2ff251b2
Abdullah <89297042+AzazelSensei@users.noreply.github.com>
2026-09-16 05:12:46 +0000

    Format the persist-await test for dart format

--- flutter_cache_manager/test/web_helper_test.dart
+++ flutter_cache_manager/test/web_helper_test.dart
@@ -274,10 +274,11 @@ void main() {
 
       final webHelper = WebHelper(store, fileService);
       var yielded = false;
-      final download = webHelper.downloadFile(imageUrl).firstWhere(
-        (r) => r is FileInfo,
-        orElse: null,
-      )..whenComplete(() => yielded = true);
+      final download =
+          webHelper
+              .downloadFile(imageUrl)
+              .firstWhere((r) => r is FileInfo, orElse: null)
+            ..whenComplete(() => yielded = true);
       await pumpEventQueue();
       expect(
         yielded,
